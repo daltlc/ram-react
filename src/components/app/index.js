9e02781f9b49ab10be21f1f902c3b89602e7ec22
Dalton Carr <daltonlcarr@yahoo.com>
2018-10-20 15:53:59 -0700

    inv working along with promotions displaying when checking out

---
+++ src/components/app/index.js
@@ -0,0 +1,73 @@
+import {Component} from 'react';
+import React from 'react';
+import { BrowserRouter ,Route, Link } from 'react-router-dom';
+import './App.css';
+import Inventory from '../inventory';
+
+class App extends Component {
+  constructor(props){
+
+    super(props);
+
+    this.state = {
+      // promotionsOK:false,
+      // shippingOK:false,
+      // checkoutOK:false,
+
+    };
+
+    // this.getPromotionsOK = this.togglePromotionsOK.bind(this);
+    // this.getShippingOK = this.toggleShippingOK.bind(this);
+    // this.getCheckoutOK = this.toggleCheckoutOK.bind(this);
+
+  }
+    // togglePromotionsOK() {
+    //   let toggleState = !this.state.inventoryOK;
+    //   this.setState({ inventoryOK: toggleState});
+    // }
+
+    // getPromotionsOK(){
+    //   return this.state.inventoryOK;
+    // }
+
+    // toggleShippingOK() {
+    //   let toggleState = !this.state.shippingOK;
+    //   this.setState({ shippingOK: toggleState});
+    // }
+
+    // getShippingOK(){
+    //   return this.state.shippingOK;
+    // }
+
+    // toggleCheckoutOK() {
+    //   let toggleState = !this.state.checkoutOK;
+    //   this.setState({ checkoutOK: toggleState});
+    // }
+
+    // getCheckoutOK(){
+    //   return this.state.checkoutOK;
+    // }
+
+  
+
+
+
+
+  render() {
+    return (
+      <div className="App">
+       <BrowserRouter>
+        <div>
+        <Route exact path='/' render={props => {
+              return <Inventory/>;
+            }}/>
+        </div>
+       </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+
+
+export default App;
---
+++ src/components/inventory/index.js
@@ -0,0 +1,92 @@
+import {Component} from 'react';
+import React from 'react';
+import { BrowserRouter ,Route, Link } from 'react-router-dom';
+import './inventory.css';
+import Checkout from '../checkout';
+
+class Inventory extends Component {
+  constructor(props){
+
+    super(props);
+
+    this.state = {};
+  }
+
+    componentWillMount() {
+
+        let request = new XMLHttpRequest();
+
+
+            //Displaying Inventory
+
+            request.open('GET', 'http://jst.edchavez.com/api/inventory/getInventory/', true);
+            request.onload = function () {
+
+
+            // Begin accessing JSON data here
+            var data = JSON.parse(this.response);
+
+            if (request.status >= 200 && request.status < 400) {
+                data.items.forEach(items => {
+                    const card = document.createElement('div');
+                    card.setAttribute('class', 'card');
+
+            // Create an h1 and set the text content to the film's title
+                    const h1 = document.createElement('h1');
+                    h1.textContent = items.name;
+
+            // Create a p and set the text content to the film's description
+                    const price = document.createElement('p');
+                    const description = document.createElement('p');
+                    const inStock = document.createElement('p');
+                    items.description = items.description.substring(0, 300); // Limit to 300 chars
+                    description.textContent = `${items.description}`; // End with an ellipses
+                    price.textContent = `${items.price}`;
+                    inStock.textContent = `InStock: ${items.inStock}`;
+
+            // Append the cards to the container element
+                    container.appendChild(card);
+
+            // Each card will contain an h1 and a p
+                    card.appendChild(h1);
+                    card.appendChild(price);
+                    card.appendChild(description);
+                    card.appendChild(inStock);
+                });
+                } else {
+                    console.log('error');
+                }
+            }
+
+                request.send();
+
+
+                const inventory = document.getElementById('inventory');
+
+                const container = document.createElement('div');
+                container.setAttribute('class','container');
+
+                inventory.appendChild(container);
+
+
+        }
+
+        
+  render() {
+    return (
+      <div className="Inventory">
+       <BrowserRouter>
+        <div>
+            <h1>Inventory</h1>
+            <Checkout/>
+            
+        </div>
+       </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+
+
+export default Inventory;
